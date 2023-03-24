aa6c3e0a575d461f2b0f9ab1a92832fef6f84e8a
Rafael <a22001810@alunos.ulht.pt>
2023-03-24 11:46:55 +0000

    -

--- app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
+++ app/src/main/java/com/example/calculadorakotlin/MainActivity.kt
@@ -56,10 +56,74 @@ class MainActivity : AppCompatActivity() {
                 binding.textVisor.append("3")
             }
         }
+        binding.button4.setOnClickListener {
+            Log.i(TAG, "Click no botão 4")
+            if (binding.textVisor.text.toString() == "0") {
+                binding.textVisor.text = "4"
+            } else {
+                binding.textVisor.append("4")
+            }
+        }
+        binding.button5.setOnClickListener {
+            Log.i(TAG, "Click no botão 5")
+            if (binding.textVisor.text.toString() == "0") {
+                binding.textVisor.text = "5"
+            } else {
+                binding.textVisor.append("5")
+            }
+        }
+        binding.button6.setOnClickListener {
+            Log.i(TAG, "Click no botão 6")
+            if (binding.textVisor.text.toString() == "0") {
+                binding.textVisor.text = "6"
+            } else {
+                binding.textVisor.append("6")
+            }
+        }
+        binding.button7.setOnClickListener {
+            Log.i(TAG, "Click no botão 7")
+            if (binding.textVisor.text.toString() == "0") {
+                binding.textVisor.text = "7"
+            } else {
+                binding.textVisor.append("7")
+            }
+        }
+        binding.button8.setOnClickListener {
+            Log.i(TAG, "Click no botão 8")
+            if (binding.textVisor.text.toString() == "0") {
+                binding.textVisor.text = "8"
+            } else {
+                binding.textVisor.append("8")
+            }
+        }
+        binding.button9.setOnClickListener {
+            Log.i(TAG, "Click no botão 9")
+            if (binding.textVisor.text.toString() == "0") {
+                binding.textVisor.text = "9"
+            } else {
+                binding.textVisor.append("9")
+            }
+        }
 
         binding.buttonAdition.setOnClickListener {
             Log.i(TAG, "Click no botão +")
             binding.textVisor.append("+")
+        }
+        binding.buttonSub.setOnClickListener {
+            Log.i(TAG, "Click no botão -")
+            binding.textVisor.append("-")
+        }
+        binding.buttonMult.setOnClickListener {
+            Log.i(TAG, "Click no botão *")
+            binding.textVisor.append("*")
+        }
+        binding.buttonAdition.setOnClickListener {
+            Log.i(TAG, "Click no botão /")
+            binding.textVisor.append("/")
+        }
+        binding.buttonAC.setOnClickListener {
+            Log.i(TAG, "Click no botão AC")
+            binding.textVisor.append("0")
         }
 
         binding.buttonEquals.setOnClickListener {
